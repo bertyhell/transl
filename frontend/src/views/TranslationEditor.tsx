9df0e394d62e1d623721862cd0e39d572c14fc4b
Bert Verhelst <verhelstbert@gmail.com>
2021-10-31 22:55:41 +0100

    fix(zeus): move generated type files to src folder

--- frontend/src/views/TranslationEditor.tsx
+++ frontend/src/views/TranslationEditor.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
 
-import { useTypedQuery } from '../../generated/zeus/reactQuery';
-import { KeyValueEditor } from '../components/KeyValueEditor/KeyValueEditor';
-import { QUERY_KEYS } from '../model/query-keys';
+// import { useTypedQuery } from '../generated/zeus/reactQuery';
+// import { KeyValueEditor } from '../components/KeyValueEditor/KeyValueEditor';
+// import { QUERY_KEYS } from '../model/query-keys';
 
 export function TranslationEditor() {
-  const translations = useTypedQuery(QUERY_KEYS.getTranslations, {
-    translations: {
-
-    },
-  })
+  // const translations = useTypedQuery(QUERY_KEYS.getTranslations, {
+  //   translations: {
+  //
+  //   },
+  // })
 
   return (
     <>
       key value editor: {}
-      <KeyValueEditor data={testTranslations} initialData={testTranslations} onChange={() => {}} />
+      {/*<KeyValueEditor data={testTranslations} initialData={testTranslations} onChange={() => {}} />*/}
     </>
   );
 }
